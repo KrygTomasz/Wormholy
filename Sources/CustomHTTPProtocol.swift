24d400326ed04a841870e077ecca9b9bf5bed4d8
Tomasz Kryg <tomasz.kryg@siroccomobile.com>
2019-12-12 12:31:33 +0100

    Remove certificate challenging

--- Sources/CustomHTTPProtocol.swift
+++ Sources/CustomHTTPProtocol.swift
@@ -23,7 +23,7 @@ public class CustomHTTPProtocol: URLProtocol {
         super.init(request: request, cachedResponse: cachedResponse, client: client)
         
         if session == nil {
-            session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+            session = URLSession(configuration: .default, delegate: nil, delegateQueue: nil)
         }
     }
     
@@ -44,21 +44,18 @@ public class CustomHTTPProtocol: URLProtocol {
         let newRequest = ((request as NSURLRequest).mutableCopy() as? NSMutableURLRequest)!
         CustomHTTPProtocol.setProperty(true, forKey: Constants.RequestHandledKey, in: newRequest)
         sessionTask = session?.dataTask(with: newRequest as URLRequest)
-        sessionTask?.resume()
         
         currentRequest = RequestModel(request: newRequest)
         Storage.shared.saveRequest(request: currentRequest)
     }
     
     override public func stopLoading() {
-        sessionTask?.cancel()
         currentRequest?.httpBody = body(from: request)
         if let startDate = currentRequest?.date{
             currentRequest?.duration = fabs(startDate.timeIntervalSinceNow) * 1000 //Find elapsed time and convert to milliseconds
         }
 
         Storage.shared.saveRequest(request: currentRequest)
-        session?.invalidateAndCancel()
     }
     
     private func body(from request: URLRequest) -> Data? {
@@ -126,20 +123,6 @@ extension CustomHTTPProtocol: URLSessionDataDelegate {
         guard let error = error else { return }
         currentRequest?.errorClientDescription = error.localizedDescription
         client?.urlProtocol(self, didFailWithError: error)
-    }
-    
-    public func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-        let protectionSpace = challenge.protectionSpace
-        let sender = challenge.sender
-        
-        if protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
-            if let serverTrust = protectionSpace.serverTrust {
-                let credential = URLCredential(trust: serverTrust)
-                sender?.use(credential, for: challenge)
-                completionHandler(.useCredential, credential)
-                return
-            }
-        }
     }
     
     public func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
